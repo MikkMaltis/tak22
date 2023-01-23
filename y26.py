490c0eaa3620bda05446450072e0b98b64418a60
MikkMaltis <maltismikk@gmail.com>
2023-01-23 08:50:44 +0200

    Unustas commitida

--- y26.py
+++ y26.py
@@ -1,9 +1,10 @@
+
 def parse_input(sales_input):
     data = inp.split("\n\n")    
     sales = {data[0].strip(): data[1], data[2].strip(): data[3]}
     for k, v in sales.items():
         rows = [[elem.strip() for elem in r.split()]
-                for r in v.splitlines()]
+        for r in v.splitlines()]
         df = pd.DataFrame(rows[1:])
         df.set_index(0, inplace=True)
         df.columns = rows[0]
@@ -17,17 +18,3 @@ def get_commission(sales_data):
     commission[commission < 0] = 0
     commission = commission.sum()
     return pd.DataFrame({"Commission": commission}).transpose()
-
-            Johnver Vanston Danbree Vansey  Mundyke
-Tea             190     140    1926     14      143
-Coffee          325      19     293   1491      162
-Water           682      14     852     56      659
-Milk            829     140     609    120       87
-
-Expenses
-
-            Johnver Vanston Danbree Vansey  Mundyke
-Tea             120      65     890     54      430
-Coffee          300      10      23    802      235
-Water            50     299    1290     12      145
-Milk             67     254      89    129       76''')
