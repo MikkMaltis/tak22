5c66ce7f8b3361f9c31dc29999c7760057a39225
MikkMaltis <maltismikk@gmail.com>
2023-01-23 08:54:58 +0200

    Commit jälle

--- y26.py
+++ y26.py
@@ -1,20 +1,26 @@
+input_list = [line.split() for line in input('Enter the input >>> ').split('\n') if line]
+input_splice = input_list.index(['Expenses'])
+commissions = dict.fromkeys(input_list[1], 0)
+revenues = {elem[0]: {name: int(revenue) for name, revenue in zip(input_list[1], elem[1:])} for elem in input_list[2: input_splice]}
 
-def parse_input(sales_input):
-    data = inp.split("\n\n")    
-    sales = {data[0].strip(): data[1], data[2].strip(): data[3]}
-    for k, v in sales.items():
-        rows = [[elem.strip() for elem in r.split()]
-        for r in v.splitlines()]
-        df = pd.DataFrame(rows[1:])
-        df.set_index(0, inplace=True)
-        df.columns = rows[0]
-        df = df.apply(pd.to_numeric)
-        sales[k] = df
-    return sales
+for elem in input_list[input_splice + 2:]:
+    for name, expense in zip(input_list[1], elem[1:]):
+        commissions[name] += max(0, revenues[elem[0]][name] - int(expense))
 
-def get_commission(sales_data):
-    profit = sales["Revenue"] - sales["Expenses"]
-    commission = profit*.062
-    commission[commission < 0] = 0
-    commission = commission.sum()
-    return pd.DataFrame({"Commission": commission}).transpose()
+print(' '12 + ' '.join(f'{key:>{max(len(key), len(str(val)))}}' for key, val in commissions.items()) + '\nCommission  ' + ' '.join(f'{str(round(val 0.062, 2)):>{max(len(key), len(str(val)))}}' for key, val in commissions.items()))
+
+Revenue
+
+            Johnver Vanston Danbree Vansey  Mundyke
+Tea             190     140    1926     14      143
+Coffee          325      19     293   1491      162
+Water           682      14     852     56      659
+Milk            829     140     609    120       87
+
+Expenses
+
+            Johnver Vanston Danbree Vansey  Mundyke
+Tea             120      65     890     54      430
+Coffee          300      10      23    802      235
+Water            50     299    1290     12      145
+Milk             67     254      89    129       76
